493dbdc4c7cae5f497c07356c3fffcfd74001169
matthias_elsner_ons <matthias.elsner@ext.ons.gov.uk>
2024-01-17 10:22:55 +0000

    added read and write authentication

--- api/api.go
+++ api/api.go
@@ -2,8 +2,16 @@ package api
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/ONSdigital/dp-authorisation/auth"
+	"github.com/ONSdigital/log.go/v2/log"
 	"github.com/gorilla/mux"
+)
+
+var (
+	read   = auth.Permissions{Read: true}
+	update = auth.Permissions{Update: true}
 )
 
 // API provides a struct to wrap the api around
@@ -19,7 +27,40 @@ func Setup(ctx context.Context, r *mux.Router, dataStore DataStore) *API {
 		dataStore: dataStore,
 	}
 
-	r.HandleFunc("/mongocheck", api.AddDataSets(ctx)).Methods("POST")
-	r.HandleFunc("/mongocheck", api.GetDataSets(ctx)).Methods("GET")
+	// GetPermissionsRequestBuilder for authorising access to datasets.
+	// datasetPermissionsRequestBuilder := auth.NewDatasetPermissionsRequestBuilder("http://localhost:8082", "dataset_id", mux.Vars)
+
+	// datasetsPermissions := auth.NewHandler(
+	// 	datasetPermissionsRequestBuilder,
+	// 	auth.DefaultPermissionsClient(),
+	// 	auth.DefaultPermissionsVerifier(),
+	// )
+
+	// GetPermissionsRequestBuilder for authorising general CMD access (cases where we don't have a collection ID & dataset ID).
+	permissionsRequestBuilder := auth.NewPermissionsRequestBuilder("http://localhost:8082")
+
+	permissions := auth.NewHandler(
+		permissionsRequestBuilder,
+		auth.DefaultPermissionsClient(),
+		auth.DefaultPermissionsVerifier(),
+	)
+
+	//r.HandleFunc("/datasets", permissions.Require(read, getDatasetsHandlerFunc)).Methods("GET")
+
+	r.HandleFunc("/mongocheck", permissions.Require(update, api.AddDataSets(ctx))).Methods("POST")
+	r.HandleFunc("/mongocheck", permissions.Require(read, api.GetDataSets(ctx))).Methods("GET")
 	return api
+}
+
+// an example http.HandlerFunc for getting a dataset
+func getDatasetsHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	log.Info(r.Context(), "get datasets stub invoked")
+	w.Write([]byte("datasets info here"))
+}
+
+// an example http.HandlerFunc for getting a dataset
+func getDatasetHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	datasetID := mux.Vars(r)["dataset_id"]
+	log.Info(r.Context(), "get dataset stub invoked", log.Data{"dataset_id": datasetID})
+	w.Write([]byte("dataset info here"))
 }
--- go.mod
+++ go.mod
@@ -18,7 +18,13 @@ require (
 )
 
 require (
+	github.com/ONSdigital/dp-rchttp v1.0.0 // indirect
+	github.com/ONSdigital/go-ns v0.0.0-20200205115900-a11716f93bad // indirect
+)
+
+require (
 	github.com/ONSdigital/dp-api-clients-go/v2 v2.254.1 // indirect
+	github.com/ONSdigital/dp-authorisation v0.2.1
 	github.com/ONSdigital/dp-mongodb-in-memory v1.7.0 // indirect
 	github.com/chromedp/cdproto v0.0.0-20231205062650-00455a960d61 // indirect
 	github.com/chromedp/chromedp v0.9.3 // indirect
