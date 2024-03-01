b6c728fed9126e8608f942d4189eb68f2418fd4a
Marilia Elia <marilia.elia@and.digital>
2024-03-01 12:31:40 +0000

    Remove omitempty from cachetime model

--- models/cachetime.go
+++ models/cachetime.go
@@ -6,5 +6,5 @@ type CacheTime struct {
 	ID           string     `bson:"_id" json:"_id"`                                         // MD5 of the path
 	Path         string     `bson:"path" json:"path"`                                       // Path for which caching is set
 	CollectionID string     `bson:"collection_id,omitempty" json:"collection_id,omitempty"` // Collection ID - used for grouping and filtering of cache-time objects.
-	ReleaseTime  *time.Time `bson:"release_time,omitempty" json:"release_time,omitempty"`   // Release time in ISO-8601 format
+	ReleaseTime  *time.Time `bson:"release_time" json:"release_time"`                       // Release time in ISO-8601 format
 }
